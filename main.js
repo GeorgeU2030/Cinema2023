0f3208825a141deebc40152322ea053f9af22bb2
George Trujillo <georgeMike2030@outlook.com>
2023-01-17 20:44:58 -0500

    Cinema-2023

---
+++ main.js
@@ -0,0 +1,69 @@
+
+
+//relink 
+
+
+function relink(id){
+
+    if(id==0){
+        location.href='/relinks/page1.html'
+    }
+    if(id==1){
+        location.href='/relinks/page2.html'
+    }
+    if(id==2){
+        location.href='/relinks/page3.html'
+    }
+    if(id==3){
+        location.href='/relinks/page4.html'
+    }
+    if(id==4){
+        location.href='/relinks/page5.html'
+    }
+    if(id==5){
+        location.href='/relinks/page6.html'
+    }
+    if(id==6){
+        location.href='/relinks/page7.html'
+    }
+    if(id==7){
+        swal('Rapidos y Furiosos 10','No hay Trailer disponible-Posible Fecha: Febrero 10\nFecha de Estreno: Mayo 19');
+    }
+    if(id==8){
+        location.href='/relinks/page9.html'
+    }
+    if(id==9){
+        location.href='/relinks/page10.html'
+    }
+    if(id==10){
+        location.href='/relinks/page11.html'
+    }
+    if(id==11){
+        location.href='/relinks/page12.html'
+    }
+    if(id==12){
+        location.href='/relinks/page13.html'
+    }
+    if(id==13){
+        location.href='/relinks/page14.html'
+    }
+    if(id==14){
+        location.href='/relinks/page15.html'
+    }
+
+    if(id==15){
+        swal('The Marvels','No hay Trailer disponible\n Fecha de Estreno: Julio 28');
+    }
+    if(id==16){
+        swal('Kraven: The Hunter','No hay Trailer disponible\n Fecha de Estreno: Octubre 6');
+    }
+    if(id==17){
+        swal('Saw X','No hay Trailer disponible\n Fecha de Estreno: Octubre 27');
+    }
+    if(id==18){
+        swal('Dune: Part Two','No hay Trailer disponible\n Fecha de Estreno: Noviembre 3');
+    }
+    if(id==19){
+        swal('Aquaman 2','No hay Trailer disponible\n Fecha de Estreno: Diciembre 25');
+    }
+}
